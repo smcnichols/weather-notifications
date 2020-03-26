27ca15e1ccdc3829ef0dbede09bc3413d54ab277
Sally McNichols <sallymcnichols@gmail.com>
2020-03-26 17:12:42 -0500

    Add Twilio text message sending to "Hello World" function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from twilio.rest import Client
+import os
+
 def hello_world(request):
     """Responds to any HTTP request.
     Args:
@@ -8,9 +11,20 @@ def hello_world(request):
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
     request_json = request.get_json()
+    message = f'Hello World!'
     if request.args and 'message' in request.args:
-        return request.args.get('message')
+        message = request.args.get('message')
     elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World!'
+        message = request_json['message']
+
+    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+    client = Client(account_sid, auth_token)
+
+    client.messages.create(
+        body=message,
+        messaging_service_sid=os.getenv('TWILIO_MESSAGING_SERVICE_SID'),
+        to=os.getenv('MY_PHONE_NUMBER')
+    )
+
+    return message
